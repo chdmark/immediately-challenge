47a685d8f1052dc2275c43de365995508b5fce77
Chris Mark <chdmark@gmail.com>
2016-01-22 15:13:35 -0800

    Extract data from messages.json. Need to change schema.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,12 +5,30 @@ class MessagesController < ApplicationController
 	end
 
 	def create
-		ap "*" * 50
-		ap params[:message][:body]
-		ap "*" *50
+		
 		parsed_data = JSON.parse(params[:message][:body])
-		ap parsed_data
+		p "*" * 50
+		
+		message_body = parsed_data["snippet"]
+		ap message_body
+		p "*" * 50
+	
 
+		p "*" * 50
+
+		ap parsed_data["payload"]["headers"]
+
+		headers = parsed_data["payload"]["headers"]
+
+		date_sent = headers[1]["value"]
+		message_author = headers[2]["value"]
+		message_recipient = headers[3]["value"]
+		message_subject = headers[5]["value"]
+		
+		ap date_sent
+		ap message_author
+		ap message_recipient
+		ap message_subject
 	
 	end
 	
