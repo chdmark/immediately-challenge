83d3c2680b91f5bb8d7559b3ec6645449ecc78cc
Chris Mark <chdmark@gmail.com>
2016-01-22 15:31:40 -0800

    Message successfully saves to database with correct data.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,26 +9,25 @@ class MessagesController < ApplicationController
 		parsed_data = JSON.parse(params[:message][:body])
 		p "*" * 50
 		
-		message_body = parsed_data["snippet"]
-		p "*" * 50
-	
-
-		p "*" * 50
-
-		ap parsed_data["payload"]["headers"]
+		
 
 		headers = parsed_data["payload"]["headers"]
 
+		message_body = parsed_data["snippet"]
 		date_sent = headers[1]["value"]
 		message_author = headers[2]["value"]
 		message_recipient = headers[3]["value"]
 		message_subject = headers[5]["value"]
 		
-		ap message_body
-		ap date_sent
-		ap message_author
-		ap message_recipient
-		ap message_subject
+		
+		message = Message.new(sender: message_author, recipient: message_recipient, date_sent: date_sent, subject: message_subject, body: message_body)
+
+		if message.save 
+			redirect_to root_path
+		else
+			flash[:error] = "Message not saved succesfully"
+			redirect_to root_path
+		end
 	
 	end
 	
