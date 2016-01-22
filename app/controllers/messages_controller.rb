3694d811906d2c1be47dc96441e53f9990e2c43b
Chris Mark <chdmark@gmail.com>
2016-01-22 14:41:14 -0800

    Correctly parse data using JSON.parse

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,17 @@
+require 'json'
+
 class MessagesController < ApplicationController
 	def index
 	end
 
 	def create
-		pp params
-		parsed_data = JSON.parse(params)
-		
+		ap "*" * 50
+		ap params[:message][:body]
+		ap "*" *50
+		parsed_data = JSON.parse(params[:message][:body])
+		ap parsed_data
+
+	
 	end
 	
 	def show
